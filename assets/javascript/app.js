b693ff31c1be4a8f3d4ff1cb0dd70a614d4713b2
Reena Mahapatra <reenam2017@gmail.com>
2019-04-08 15:38:12 -0400

    modified menu-icon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   console.log("Document loaded");
   $(".menu-icon").on("click", function() {
-    $("nav ul").toggleClass("showing");
+    $(".menu-1").toggleClass("showing");
+    $(".close-icon").show();
+    $(".menu-icon").hide();
   });
+  $(".close-icon").on("click", function() {
+    $(".menu-1").toggleClass("showing");
+    $(".menu-icon").show();
+    $(".close-icon").hide();
+  });
+  // $('#workHover').hover(
+  //   function(){
+  //     $('.showing').
+  //   }
+  // )
 });
